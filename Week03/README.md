b839a839e48fca1b3cea91003858c1c8f69dce81
h1326432154 <m15191488150@163.com>
2020-12-03 14:58:30 +0800

    第三周预习

--- Week03/README.md
+++ Week03/README.md
@@ -1,1 +1,15 @@
-学习笔记
+# 并发(concurrency)
+
+## Goroutine
+
+如果包p引入（import）包q，那么q的init函数的结束先行发生于p的所有init函数开始 main.main函数的开始发生在所有init函数结束之后
+
+## Memory model
+
+## Package sync
+
+## chan
+
+## Package context
+
+## References
---
+++ Week03/main.go
@@ -0,0 +1,55 @@
+// basic is a program used to define a goroutine leak.
+package main
+
+// func main() {
+
+// 	// Capture starting number of goroutines.
+// 	startingGs := runtime.NumGoroutine()
+
+// 	leak()
+
+// 	// Hold the program from terminating for 1 second to see
+// 	// if any goroutines created by leak terminate.
+// 	time.Sleep(time.Second)
+
+// 	// Capture ending number of goroutines.
+// 	endingGs := runtime.NumGoroutine()
+
+// 	// Report the results.
+// 	fmt.Println("========================================")
+// 	fmt.Println("Number of goroutines before:", startingGs)
+// 	fmt.Println("Number of goroutines after :", endingGs)
+// 	fmt.Println("Number of goroutines leaked:", endingGs-startingGs)
+// }
+
+// // leak is a buggy function. It launches a goroutine that
+// // blocks receiving from a channel. Nothing will ever be
+// // sent on that channel and the channel is never closed so
+// // that goroutine will be blocked forever.
+// func leak() {
+// 	ch := make(chan int)
+
+// 	go func() {
+// 		val := <-ch
+// 		fmt.Println("We received a value:", val)
+// 	}()
+// }
+
+var a string
+
+/*
+// --- go关键字开启新的goroutine，先行发生于这个goroutine开始执行
+func f() {
+	fmt.Println(a, "111")
+}
+
+func main() {
+	a = "hello, world"
+	go f()
+}
+*/
+
+func hello() {
+	go func() { a = "hello" }()
+	print(a)
+}
