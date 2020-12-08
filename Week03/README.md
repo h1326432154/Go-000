abe8df2751a658c51143e7dbfb2b151082a6c2e0
h1326432154 <m15191488150@163.com>
2020-12-08 15:55:07 +0800

    内存模型

--- Week03/README.md
+++ Week03/README.md
@@ -1,6 +1,6 @@
 # 并发(concurrency)
 
-> Concurrency is not Parallelism.
+> Don’t communicate by sharing memory; share memory by communicating.
 
 ## Goroutine
 
@@ -18,9 +18,19 @@
 
     把握goroutine生命周期
 
-## Memory model
+## Memory model [内存模型](https://www.jianshu.com/p/97a345f47cfd)
 
-* [内存模型](https://www.jianshu.com/p/5e44168f47a3)
+> Go内存模型指定条件，在该条件下，可以保证一个goroutine中的变量读取可以观察到不同goroutine写入同一个变量而产生的值
+
+注：任何一个Go程序中都不会只有一个goroutine的存在，即使你没有显示声明过（go关键字声明），程序在启动时除了有一个main的goroutine存在之外，至少还会隐式的创建一个goroutine用于gc，使用runtime.NumGoroutine()可以得到程序中的goroutine的数量
+
+### Happens Before原则
+
+* 如果事件e1发生在e2之前，那么我们就可以说事件e2发生在e1之后
+* 如果e1既不发生在e2之前，也不发生在e2之后，那么我们就说e1和e2是同时发生的
+
+同步(Synchronization)
+初始化(Initialization)
 
 * CPU 指令重排
 
@@ -29,6 +39,21 @@
 ## Package sync
 
 ## chan
+
+channel通信是goroutine同步的主要方法。每一个在特定channel的发送操作都会匹配到通常在另一个goroutine执行的接收操作。
+
+> 一个通道相当于一个先进先出（FIFO）的队列。也就是说，通道中的各个元素值都是严格地按照发送的顺序排列的，先被发送通道的元素值一定会先被接收。元素值的发送和接收都需要用到操作符<-。我们也可以叫它接送操作符。一个左尖括号紧接着一个减号形象地代表了元素值的传输方向。
+
+* 对于同一个通道，发送操作之间是互斥的，接收操作之间也是互斥的。
+* 发送操作和接收操作中对元素值的处理都是不可分割的。
+* 发送操作在完全完成之前会被阻塞。接收操作也是如此。
+
+元素值从外界进入通道时会被`复制`。更具体地说，进入通道的并不是在接收操作符右边的那个元素值，而是它的副本。
+另一方面，元素值从通道进入外界时会被`移动`。这个移动操作实际上包含了两步，第一步是生成正在通道中的这个元素值的副本，并准备给到接收方，第二步是删除在通道中的这个元素值。
+
+>注:如果通道关闭时，里面还有元素值未被取出，那么接收表达式的第一个结果，仍会是通道中的某一个元素值，而第二个结果值一定会是true。
+
+所以除非有特殊的保障措施，不要让接收方关闭通道，而应当让发送方做这件事。
 
 ## Package context
 
--- Week03/main.go
+++ Week03/main.go
@@ -2,22 +2,21 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
+	"sync/atomic"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "hello,GopherCon SG")
-	})
-	go func() {
-		if err := http.ListenAndServe("8080", nil); err != nil {
-			log.Fatal(err)
-		}
-	}()
+// func main() {
+// 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+// 		fmt.Fprintln(w, "hello,GopherCon SG")
+// 	})
+// 	go func() {
+// 		if err := http.ListenAndServe("8080", nil); err != nil {
+// 			log.Fatal(err)
+// 		}
+// 	}()
 
-	select {}
-}
+// 	select {}
+// }
 
 // func main() {
 
@@ -53,9 +52,10 @@ func main() {
 // 	}()
 // }
 
+// ### memory model
+/*
 var a string
 
-/*
 // --- go关键字开启新的goroutine，先行发生于这个goroutine开始执行
 func f() {
 	fmt.Println(a, "111")
@@ -65,9 +65,232 @@ func main() {
 	a = "hello, world"
 	go f()
 }
-*/
+// */
 
+/*
 func hello() {
 	go func() { a = "hello" }()
 	print(a)
+}
+
+func main() {
+	a = "hello, world"
+	go hello()
+}
+*/
+
+/*
+var c = make(chan int, 10)
+
+func f() {
+	a = "hello, world"
+	c <- 0
+}
+
+func main() {
+	go f()
+	<-c
+	print(a)
+}
+*/
+
+/*
+var c = make(chan int)
+
+func f() {
+	a = "hello, world"
+	<-c
+}
+
+func main() {
+	go f()
+	c <- 0
+	print(a)
+}
+*/
+
+/*
+var l sync.RWMutex
+var a string
+
+func f() {
+	a = "hello, world"
+	l.Unlock()
+}
+
+func main() {
+	l.Lock()
+	go f()
+	l.Lock()
+	print(a)
+}
+*/
+
+/*
+var a string
+var once sync.Once
+
+func setup() {
+	println("aaa")
+	a = "hello, world"
+}
+
+func doprint() {
+	once.Do(setup)
+	once.Do(setup)
+	println(a)
+}
+
+func main() {
+	go doprint()
+	go doprint()
+	time.Sleep(time.Millisecond)
+}
+*/
+
+/*
+var a, b int
+
+func f() {
+	a = 1
+	b = 2
+
+}
+
+func g() {
+	print(b)
+	print(a)
+}
+
+func main() {
+	go f()
+	g()
+}
+
+*/
+
+/* 错误示范
+var a string
+var done bool
+var once sync.Once
+
+func setup() {
+	a = "hello, world"
+	done = true
+}
+
+func doprint() {
+	if !done {
+		once.Do(setup)
+	}
+	print(a)
+}
+
+func main() {
+	go doprint()
+	go doprint()
+}
+
+*/
+
+/*
+var a string
+var done bool
+
+func setup() {
+	a = "hello, world"
+	done = true
+}
+func main() {
+	go setup()
+	for !done {
+	}
+	print(a)
+}
+*/
+
+/*
+// T .
+type T struct {
+	msg string
+}
+
+var g *T
+
+func setup() {
+	t := new(T)
+	t.msg = "hello, world"
+	g = t
+}
+func main() {
+	go setup()
+	for g == nil {
+	}
+	print(g.msg)
+}
+*/
+
+/*
+func main() {
+	// 准备好几个通道。
+	intChannels := [3]chan int{
+		make(chan int, 1),
+		make(chan int, 1),
+		make(chan int, 1),
+	}
+	// 随机选择一个通道，并向它发送元素值。
+	rand.Seed(time.Now().UnixNano())
+	index := rand.Intn(2)
+	fmt.Printf("The index: %d\n", index)
+	intChannels[index] <- index
+	// 哪一个通道中有可取的元素值，哪个对应的分支就会被执行。
+	select {
+	case <-intChannels[0]:
+		fmt.Println("The first candidate case is selected.")
+	case <-intChannels[1]:
+		fmt.Println("The second candidate case is selected.")
+	case elem := <-intChannels[2]:
+		fmt.Printf("The third candidate case is selected, the element is %d.\n", elem)
+	default:
+		fmt.Println("No candidate case is selected!")
+	}
+}
+*/
+
+/*
+func main() {
+	ch := make(chan struct{})
+	for i := 0; i < 100; i++ {
+		go func(i int) {
+			fmt.Println(i)
+			ch <- struct{}{}
+		}(i)
+		<-ch
+	}
+}
+*/
+
+func main() {
+	var count uint32
+
+	trigger := func(i uint32, fn func()) {
+		for {
+			if n := atomic.LoadUint32(&count); n == i {
+				fn()
+				atomic.AddUint32(&count, 1)
+				break
+			}
+		}
+	}
+
+	for i := uint32(0); i < 10; i++ {
+		go func(i uint32) {
+			fn := func() {
+				fmt.Println(i)
+			}
+			trigger(i, fn)
+		}(i)
+	}
+	trigger(10, func() {})
+
 }
